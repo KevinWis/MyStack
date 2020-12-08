48293da32161a55f182ae78a02366c1724444e3b
Eliabe Santos <eliabedodo@gmail.com>
2020-12-08 19:06:24 -0300

    placeholder do member card Realizado

--- src/components/members/memberCard/index.jsx
+++ src/components/members/memberCard/index.jsx
@@ -1,5 +1,80 @@
+import {
+  DefaultCard,
+  CardName,
+  CardTech,
+  CardContainer,
+  TechContainer,
+  ImageContainer,
+  PersonContainer,
+  PlaceholdeImg,
+} from "./style.js";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    backgroundColor: "#43c1d8",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(19),
+    fontWeight: theme.typography.fontWeightBold,
+  },
+}));
+
 const MemberCard = () => {
-  return <></>;
+  const classes = useStyles();
+  return (
+    <>
+      <DefaultCard>
+        <PersonContainer>
+          <ImageContainer>
+            <PlaceholdeImg />
+          </ImageContainer>
+          <CardContainer>
+            <CardName>
+              <img></img>
+              <p></p>
+            </CardName>
+            <CardTech>
+              <img></img>
+              <p></p>
+            </CardTech>
+          </CardContainer>
+        </PersonContainer>
+        <TechContainer>
+          <Accordion className={classes.root}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography className={classes.heading}>Tec 1</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>PlaceHolder</Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion className={classes.root}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography className={classes.heading}>Tec 2</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>PlaceHolder</Typography>
+            </AccordionDetails>
+          </Accordion>
+        </TechContainer>
+      </DefaultCard>
+    </>
+  );
 };
 
 export default MemberCard;
--- src/components/members/memberCard/style.js
+++ src/components/members/memberCard/style.js
@@ -1,3 +1,70 @@
-import { styled } from "styled-components";
+import styled from "styled-components";
 
-export const defaultStyle = styled.div``;
+export const DefaultCard = styled.div`
+  width: 35rem !important;
+  height: 20rem !important;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #43c1d8 !important;
+  border-radius: 3px;
+`;
+
+export const CardContainer = styled.div`
+  height: 100%;
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  div {
+    margin: 1rem 0 0 0;
+  }
+`;
+
+export const TechContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ImageContainer = styled.div`
+  width: 40%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const PersonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const CardName = styled.div`
+  width: 80% !important;
+  height: 2rem !important;
+  background-color: white;
+  text-align: center;
+  border-radius: 3px;
+  line-height: 2px;
+  img {
+    width: 10px;
+  }
+`;
+
+export const CardTech = styled.div`
+  width: 80% !important;
+  height: 2rem !important;
+  background-color: white;
+  text-align: center;
+  border-radius: 3px;
+  line-height: 2px;
+`;
+
+export const PlaceholdeImg = styled.div`
+  width: 8rem;
+  height: 8rem;
+  margin-top: 5px;
+  background-color: gray;
+  border-radius: 50%;
+`;
