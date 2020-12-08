2a7173c7ffcba2d0fb923f1e88455591405269f2
kevin wischneski <kevinwischneski@gmail.com>
2020-12-08 17:37:48 -0300

    fixing import of welcome svg in imports

--- src/helpers/getImages.js
+++ src/helpers/getImages.js
@@ -4,7 +4,7 @@ import Employ from "../images/employ.svg";
 import Freelance from "../images/freelance.svg";
 import OpenSource from "../images/openSource.svg";
 import SoftwareDeveloper from "../images/softwareDeveloper.svg";
-import Welcome from "../images/Welcome.svg";
+import Welcome from "../images/welcome.svg";
 import ImageComponent from "../components/shared/imageComponent";
 
 export const AnnounceImage = () => {
