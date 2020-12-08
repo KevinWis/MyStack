d415c54cf92460c1d1a0e10220ab3d28f9fe79af
Eliabe Santos <eliabedodo@gmail.com>
2020-12-08 19:47:55 -0300

    development preparado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import Login from "./pages/login";
-
 function App() {
   return (
     <div className="App">
-      <main>
-        <Login />
-      </main>
+      <main></main>
     </div>
   );
 }
