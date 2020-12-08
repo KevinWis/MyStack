1d41325f7ab9d14adaf56f25da9190bd65cc27d8
Eliabe Santos <eliabedodo@gmail.com>
2020-12-08 15:24:20 -0300

    posicao e cores do Header configuradas

--- src/components/shared/header/style.js
+++ src/components/shared/header/style.js
@@ -2,11 +2,14 @@ import styled from "styled-components";
 
 export const HeaderDiv = styled.div`
   width: 100%;
-  background-color: white;
+  position: fixed;
+  top: 0px;
+  background-color: whitesmoke;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  border-bottom: 2px solid rgba(0, 0, 0, 0.2);
+  z-index: 99;
+  box-shadow: 0px 0px 3px rgba(50, 50, 50, 0.77);
   h1 {
     margin: 10px;
   }
