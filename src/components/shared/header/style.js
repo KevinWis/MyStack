7f37f2e7904faea43a214874a481558c6a526cbf
Eliabe Santos <eliabedodo@gmail.com>
2020-12-08 13:41:49 -0300

    component Header criado e estilizado

--- src/components/shared/header/style.js
+++ src/components/shared/header/style.js
@@ -1,3 +1,13 @@
-import { styled } from "styled-components";
+import styled from "styled-components";
 
-export const defaultStyle = styled.div``;
+export const HeaderDiv = styled.div`
+  width: 100%;
+  background-color: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border-bottom: 2px solid rgba(0, 0, 0, 0.2);
+  h1 {
+    margin: 10px;
+  }
+`;
