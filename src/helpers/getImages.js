49e26f98b845f042cdfb9300d4f97bc4c48cfc91
kevin wischneski <kevinwischneski@gmail.com>
2020-12-08 16:41:11 -0300

    imagens importadas para o helper

---
+++ src/helpers/getImages.js
@@ -0,0 +1,7 @@
+import AnnounceImage from "../images/announce.svg";
+import ColabImage from "../images/colab.svg";
+import EmployImage from "../images/employ.svg";
+import FreelanceImage from "../images/freelance.svg";
+import OpenSourceImage from "../images/openSource.svg";
+import SoftwareDeveloperImage from "../images/SoftwareDeveloper.svg";
+import WelcomeImage from "../images/Welcome.svg";
