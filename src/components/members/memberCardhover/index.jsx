020eeb3ddf2c7b673b68fc656942b0d253d51a51
BrunoGehlen <tousepc@gmail.com>
2020-12-09 20:08:02 -0300

    Paired commit

--- src/components/members/memberCardhover/index.jsx
+++ src/components/members/memberCardhover/index.jsx
@@ -1,33 +1,77 @@
 import {
-  MainCardContainer,
-  ImgContainer,
-  CardContainer,
-  PersonContainer,
-  DescContainer,
-  TechIcons,
-  FullimageContaner,
+  Card,
+  Body,
+  ProfileImage,
+  Medias,
+  Media,
+  Footer,
+  ImageFilter
 } from "./style";
 
-const memberCardhover = () => {
+import { AiOutlineHeart,AiFillInstagram,AiFillYoutube, AiFillGithub } from 'react-icons/ai'
+import { ImLinkedin }from 'react-icons/im'
+import { FaRegComment } from 'react-icons/fa'
+import { TiArrowForwardOutline } from 'react-icons/ti'
+import { useState } from 'react'
+
+const MemberCardhover = () => {
+
+  const [medias,setMedias] = useState([
+    Math.floor((Math.random() + 1) * 10),
+    Math.floor((Math.random() + 1) * 10)
+  ])
+
   return (
     <>
-      <MainCardContainer>
-        <CardContainer>
-          <FullimageContaner>
-            <DescContainer>
-              <h3>Nome</h3>
-            </DescContainer>
-          </FullimageContaner>
-          <PersonContainer></PersonContainer>
-          <TechIcons>
-            <p>teste</p>
-            <p>teste</p>
-            <p>teste</p>
-          </TechIcons>
-        </CardContainer>
-      </MainCardContainer>
+      <Card>
+        <Body>
+            <ProfileImage>
+              <ImageFilter>
+                <div className='Dev_Name'>
+                  <p>Dev's Name</p>
+                </div>
+              </ImageFilter>
+            </ProfileImage>
+            <Medias>
+
+                <Media >
+                  <AiFillGithub className='Media_Icon'size='2.8rem'/>
+                </Media>
+
+  
+                <Media>
+                  <ImLinkedin className='Media_Icon'size='2.4rem'/>
+                </Media>
+
+  
+                <Media>
+                  <AiFillInstagram className='Media_Icon'size='2.8rem'/>
+                </Media>
+
+                <Media>
+                  <AiFillYoutube className='Media_Icon'size='2.8rem'/>
+                </Media>
+              </Medias>
+        </Body>
+        <Footer>
+          <div className='Icon'>
+            <AiOutlineHeart size='32px'/>
+          </div>
+            <p>{`${medias[0]}.${medias[1]}`} k</p>
+          <div className='Spacer'></div>
+          <div className='Icon'>
+            <FaRegComment size='32px'/>
+          </div>
+          <p>{`${medias[0]}.${medias[1]}`} k</p>
+          <div className='Spacer'></div>
+          <div className='Icon'>
+            <TiArrowForwardOutline size='32px'/>
+          </div>
+          <p>Profile</p>
+        </Footer>
+      </Card>
     </>
   );
 };
 
-export default memberCardhover;
+export default MemberCardhover;
--- src/components/members/memberCardhover/style.js
+++ src/components/members/memberCardhover/style.js
@@ -1,57 +1,120 @@
 import styled from "styled-components";
-export const MainCardContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: #ccc;
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  padding-top: 4rem;
-`;
-export const CardContainer = styled.div`
-  width: 22rem;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-export const PersonContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  box-shadow: 4px 4px 2px rgba(0, 0, 0, 0.4);
-`;
-export const ImgContainer = styled.div`
-  width: 10rem;
-  height: 10rem;
-  background-color: black;
-  border-radius: 100%;
-  transform: translatey(5rem);
-  box-shadow: 4px 4px 2px rgba(0, 0, 0, 0.4);
-`;
-export const FullimageContaner = styled.div`
-  width: 100%;
-  height: 10rem;
-  background-color: black;
+import Egirl from './placeholders/e-girl.png'
+
+export const Card = styled.div `
+  width: 344px;
+  height: 372px;
+  margin:auto;
+  background-color: whitesmoke;
+  box-shadow: 2px 4px 7px rgba(21, 64, 72, 0.6);
+  margin-bottom:25px;
 `;
 
-export const DescContainer = styled.div`
-  width: 100%;
-  height: 7rem;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 5rem;
-  p {
-    margin: 0;
-  }
+export const Body = styled.div `
+width:100%;
+height:288px;
+display:block;
 `;
-export const TechIcons = styled.div`
-  width: 100%;
-  height: 5rem;
-  background-color: green;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
+
+export const ProfileImage = styled.div `
+width:100%;
+height:288px;
+background-image: url(${Egirl});
+background-size: cover;
+
+`;
+export const ImageFilter = styled.div `
+width:100%;
+height:288px;
+background-color:rgba(21, 64, 72, 0.75);
+.Dev_Name{
+  display:flex;
+  position:relative;
+  top:25%;
+  justify-content:center;
+  width:100%;
+  font-size:24px;
+  font-weight: Thin 100;
+  color:#E4F6F9;
+}
+`
+
+export const Medias = styled.div `
+display: flex;
+width: 100%;
+height: 372px;
+margin-top: -340px;
+justify-content: space-evenly;
+opacity: 0;
+transform: translateY(65px);
+transition:transform 0.8s ease-out;
+
+
+
+&:hover {
+  :nth-child(1) Media {
+    transition-delay:0.2s;
+  }
+  
+  :nth-child(1) Media {
+      transition-delay:0.2s;
+  }
+  /* :nth-child(2){
+    transition-delay:0.5s
+  }
+
+  :nth-child(3){
+    transition-delay:1s
+  }
+
+  :nth-child(4){
+    transition-delay:1.5s
+  } */
+
+  transform: translateY(0px);
+  opacity:1;
+}
+`;
+
+export const Media = styled.div `
+width:3.7rem;
+height:3.7rem;
+margin-top: 250px;
+background-color: whitesmoke;
+border-radius:50%;
+box-shadow:4px 6px 7px;
+z-index:100;
+display:flex;
+align-items:center;
+.Media_Icon{
+  margin:auto;
+}
+`;
+
+export const Footer = styled.div `
+display: flex;
+margin:auto;
+margin-top:24px;
+width:90%;
+height:6rem;
+justify-content:space-evenly;
+font-size:16px;
+.Spacer{
+  position: relative;
+  border-left:1px solid gray;
+  height:40px;
+  filter: opacity(0.5);
+}
+p{
+  margin-top: 8px;
+}
+.Icon{
+  filter: opacity(0.5);
+}
+
+div{
+  position: relative;
+  top:0%;
+}
+
 `;
