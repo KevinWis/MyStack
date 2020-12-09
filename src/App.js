4e79d6e0068076d1f9aa1e0fa5410e846e2e2935
Eliabe Santos <eliabedodo@gmail.com>
2020-12-09 18:51:47 -0300

    header e footer adicionados diretamente no App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Routes from "./routes";
+import Header from "./components/shared/header";
+import Footer from "./components/shared/footer";
 
 function App() {
   return (
     <div className="App">
       <main>
+        <Header />
         <Routes />
+        <Footer />
       </main>
     </div>
   );
