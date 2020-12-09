5bbd81200eb194a1f12aa9339052518e1f72e487
kevin wischneski <kevinwischneski@gmail.com>
2020-12-09 12:38:28 -0300

    Icon buttonIcon button, menu and menu items added, handlers for the clicks on the items plus the history push to work with pages also added

--- src/components/shared/header/index.jsx
+++ src/components/shared/header/index.jsx
@@ -1,12 +1,69 @@
 import { HeaderDiv } from "./style";
-import Img from "./img.svg";
+import { IconButton } from "@material-ui/core";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Header = () => {
+  const history = useHistory();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMenuItemClick = (url) => {
+    handleClose();
+    history.push(url);
+  };
+
   return (
     <>
       <HeaderDiv>
         <h1>My Dev</h1>
-        <img src={Img} />
+        <div>
+          <IconButton onClick={handleClick}>
+            <GiHamburgerMenu />
+          </IconButton>
+          <Menu
+            id="hamburger"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem
+              onClick={() => {
+                handleMenuItemClick("/");
+              }}
+            >
+              Home
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                handleMenuItemClick("/");
+              }}
+            >
+              PlaceHolder2
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                handleMenuItemClick("/");
+              }}
+            >
+              PlaceHolder3
+            </MenuItem>
+          </Menu>
+        </div>
       </HeaderDiv>
     </>
   );
--- src/components/shared/header/style.js
+++ src/components/shared/header/style.js
@@ -1,16 +1,23 @@
 import styled from "styled-components";
 
 export const HeaderDiv = styled.div`
-  width: 100%;
+  width: calc(100% - 2rem);
+
+  padding: 0 1rem;
+
   position: fixed;
   top: 0px;
   background-color: whitesmoke;
+
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+
+  align-items: center;
   z-index: 99;
-  box-shadow: 0px 0px 3px rgba(50, 50, 50, 0.77);
+  box-shadow: 0px 0px 3px rgba(50, 50, 50, 0.75);
+
   h1 {
-    margin: 10px;
+    margin: 0.8rem;
   }
 `;
