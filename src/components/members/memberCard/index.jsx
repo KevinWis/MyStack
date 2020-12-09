336aba0be306c21fab5d34bc9745a68b3d68cab4
Eliabe Santos <eliabedodo@gmail.com>
2020-12-09 17:39:35 -0300

    funcoes adicionadas ao deafaut button

--- src/components/members/memberCard/index.jsx
+++ src/components/members/memberCard/index.jsx
@@ -1,5 +1,34 @@
-const MemberCard = () => {
-  return <></>;
+import {
+  MainCardContainer,
+  CardContainer,
+  ImgContainer,
+  DescContainer,
+  TechIcons,
+  PersonContainer,
+} from "./style";
+import AppleIcon from "@material-ui/icons/Apple";
+
+const MemberCard = ({ MemberName, MemberWork }) => {
+  return (
+    <>
+      <MainCardContainer>
+        <CardContainer>
+          <ImgContainer />
+          <PersonContainer>
+            <DescContainer>
+              <h3>{MemberName}</h3> <br />
+              <p>{MemberWork}</p>
+            </DescContainer>
+            <TechIcons>
+              <AppleIcon fontSize="large" />
+              <AppleIcon fontSize="large" />
+              <AppleIcon fontSize="large" />
+            </TechIcons>
+          </PersonContainer>
+        </CardContainer>
+      </MainCardContainer>
+    </>
+  );
 };
 
 export default MemberCard;
--- src/components/shared/buttons/defaultButton/index.jsx
+++ src/components/shared/buttons/defaultButton/index.jsx
@@ -1,11 +1,15 @@
 import { DefaultSharedButton } from "./style.js";
+import { useHistory } from "react-router-dom";
 
-const DefaultButton = ({ value }) => {
+const DefaultButton = ({ value, _onType, _onClick }) => {
+  const history = useHistory();
   return (
     <DefaultSharedButton
       className="default-button"
       variant="outlined"
       color="primary"
+      type={_onType}
+      onClick={_onClick}
     >
       {value}
     </DefaultSharedButton>
