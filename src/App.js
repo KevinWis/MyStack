652d6fa4dc85a65d91da7921723577e2de5129b4
kevin wischneski <kevinwischneski@gmail.com>
2020-12-09 16:08:46 -0300

    fixing problems in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Header from "./components/shared/header";
-function App() {
-  return (
-    <div className="App">
-      <main>
-        <Header />
-      </main>
-=======
 function App() {
   return (
     <div className="App">
       <main></main>
->>>>>>> c205d5c988034ad1081a2dffaa8ac94958f75b39
-=======
-function App() {
-  return (
-    <div className="App">
-      <main></main>
->>>>>>> 23afe1807c9d89413ba3897b6ea21beaf8d6da54
     </div>
   );
 }
