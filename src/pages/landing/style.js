8fba8f29a3d3c2dcc37bfadf1e9f7425012a601c
kevin wischneski <kevinwischneski@gmail.com>
2020-12-10 10:09:05 -0300

    landing page works in both mobile and desktop, poppins font works correctly, buttons adjusted. footer and header have beeen modified to work in this particular page. Might cause bugs in other pages that need to be fixed

--- src/pages/landing/style.js
+++ src/pages/landing/style.js
@@ -18,22 +18,44 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
   background-color: whitesmoke;
+  h1 {
+    margin-bottom: 1rem;
+  }
+  @media (min-width: 640px) {
+    img {
+      margin: 4rem;
+    }
+  }
 `;
+export const InnerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-around;
 
+  width: 100%;
+  @media (min-width: 640px) {
+    flex-direction: row;
+  }
+`;
 export const ButtonsContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 60%;
   margin: 0 auto;
   justify-content: space-between;
 
   button {
     margin-top: 2rem;
+    width: 100%;
   }
+
   @media (min-width: 640px) {
     width: 40%;
-  }
-  @media (min-width: 1080px) {
-    width: 30%;
+    margin: 3rem;
+    button {
+      width: 80%;
+    }
   }
 `;
