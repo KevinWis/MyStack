fbe2bc1ecda574820d399cdfbe2cea4213cea3cc
DiegoPires <programadorpires@gmail.com>
2020-12-10 08:17:23 -0300

    Retirado o import do app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import MemberCardhover from "./components/members/memberCardhover"
-
 function App() {
   return (
     <div className="App">
       <main>
-     <MemberCardhover></MemberCardhover>
       </main>
     </div>
   );
