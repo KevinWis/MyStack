902645b4f482c002b80828768d0f0f3dd6f17c56
DiegoPires <programadorpires@gmail.com>
2020-12-11 14:32:21 -0300

    Feito o dicionario de icons

---
+++ src/helpers/geticons.js
@@ -0,0 +1,50 @@
+import Bootstrap from "../helpers/icons/icons8-bootstrap.svg";
+import Icons8c from "../helpers/icons/icons8-c++.svg";
+import Java from "../helpers/icons/icons8-java.svg";
+import Jquery from "../helpers/icons/icons8-jquery.svg";
+import Material  from "../helpers/icons/icons8-material-ui.svg";
+import Python from "../helpers/icons/icons8-python.svg";
+import Ruby from "../helpers/icons/icons8-ruby-programming-language.svg";
+import Typescript from "../helpers/icons/icons8-typescript.svg";
+import Vue from "../helpers/icons/icons8-vue-js.svg";
+
+
+export const dictionaryIcons = [
+  {
+      name:"bootstrap ",
+      img:Bootstrap,
+  },
+  {
+    name:"c++",
+    img:Icons8c,
+},
+{
+  name:"java",
+  img:Java,
+},
+{
+  name:"jquery",
+  img:Jquery,
+},
+{
+  name:"material",
+  img:Material,
+},
+{
+  name:"python",
+  img:Python,
+},
+{
+  name:"ruby",
+  img:Ruby,
+},
+{
+  name:"typescript",
+  img:Typescript,
+},
+{
+  name:"vue",
+  img:Vue,
+},
+
+]
