f795fe504df340e89b9fcc2b142be48a325a0a61
Bruno Gehlen <tousepc@gmail.com>
2020-12-12 16:08:40 -0300

    Feat: Add UPDATE tech request, and UPDATE work request

---
+++ src/kenzieHub/works/updateWork.js
@@ -0,0 +1,20 @@
+import kenzieHub from '../../services/axios'
+
+export const updateWork = async(newWorkInfo) => {
+
+    const {title,description,workID} = newWorkInfo
+    const  token = localStorage.getItem('authToken')
+    
+    try {
+        const response = await kenzieHub.put(`/users/works/${workID}`,{
+            "title":title ? title : "",
+            "description": description ?  description : "" 
+        },{
+            headers:{
+                Authorization: `Bearer ${token}`
+        }
+    })
+    } catch (error) {
+        console.log(error)
+    }
+}
