befd40c19440b6d16f3b26e74db5a1271f607f3f
Bruno Gehlen <tousepc@gmail.com>
2020-12-12 14:47:14 -0300

    Feat: Add DELETE requests

---
+++ src/kenzieHub/works/deleteWork.js
@@ -0,0 +1,15 @@
+import kenzieHub from '../../services/axios'
+
+export const deleteWork = async(workID) => {
+
+    const  token = localStorage.getItem('authToken')
+    
+    try {
+        const response = await kenzieHub.delete(`/users/works/${workID}`,{
+            headers:{Authorization: `Bearer ${token}`
+        }
+    })
+    } catch (error) {
+        console.log(error)
+    }
+}
