0cfb1814906b2711fe28c2969e520d3ad4a1e1e0
BrunoGehlen <tousepc@gmail.com>
2020-12-13 02:12:32 -0300

    Feat: infinite Scroling

--- src/pages/members/index.jsx
+++ src/pages/members/index.jsx
@@ -1,25 +1,55 @@
 import CardHover from "../../components/members/memberCardhover";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { getUserListThunk } from "../../store/modules/members/thunks";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { MemberList } from './style'
+import MemberCard from "../../components/members/memberCard";
 
 const Members = () => {
+  
   const dispatch = useDispatch();
   const members = useSelector((state) => state.members.list);
-  console.log(members);
+
+  const [nextPage, setNextPage] = useState(1)
+  
+  const updatePage = () => setNextPage(nextPage + 1)
+  
   useEffect(() => {
-    dispatch(getUserListThunk({ perPage: 15, page: 1 }));
-  }, []);
+    dispatch(getUserListThunk({ perPage: 15, page: nextPage }));
+  }, [nextPage]);
+  
   return (
     <>
-      {members.map(({ name, id, course_module }, index) => (
-        <CardHover
-          key={index}
-          name={name}
-          id={id}
-          course={course_module}
-        ></CardHover>
-      ))}
+    <InfiniteScroll
+      style={{height: "auto", overflow: "hidden"}}
+      dataLength={members.length}
+      next={updatePage}
+      loader={<h4>Get a real loader...</h4>}
+      hasMore={true}
+      >
+        {members.map(({ name, id, course_module,avatar_url }, index) => (
+            <MemberCard
+              key={index}
+              MemberName={name}
+              id={id}
+              MemberWork={course_module}
+              _MemberImg={avatar_url}
+            ></MemberCard>
+          ))}
+        <MemberList>
+          
+          {members.map(({ name, id, course_module,avatar_url }, index) => (
+            <CardHover
+              key={index}
+              name={name}
+              id={id}
+              course={course_module}
+              avatar={avatar_url}
+            ></CardHover>
+          ))}
+        </MemberList>
+      </InfiniteScroll>
     </>
   );
 };
--- src/pages/members/style.js
+++ src/pages/members/style.js
@@ -1,2 +1,10 @@
 import styled from "styled-components";
 
+export const MemberList = styled.div `
+    width: 80%;
+    margin: auto;
+    margin-top:6rem;
+    overflow: hidden !important;
+    display: flex;
+    flex-flow: row wrap;
+`
