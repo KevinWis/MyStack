cd52a54556fcaa5bcfd6d2cecb4c75ee6d6d4064
kevin wischneski <kevinwischneski@gmail.com>
2020-12-14 17:58:02 -0300

    adding state

--- src/pages/members/index.jsx
+++ src/pages/members/index.jsx
@@ -2,47 +2,60 @@ import CardHover from "../../components/members/memberCardhover";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState, useMemo } from "react";
 import { getUserListThunk } from "../../store/modules/members/thunks";
-import InfiniteScroll from 'react-infinite-scroll-component';
-import { MemberList } from './style'
+import InfiniteScroll from "react-infinite-scroll-component";
+import { MemberList } from "./style";
 import MemberCard from "../../components/members/memberCard";
 
 const Members = () => {
-  
+  //const [];
+
   const dispatch = useDispatch();
   const members = useSelector((state) => state.members.list);
 
-  const [nextPage, setNextPage] = useState(1)
-  
-  const updatePage = () => setNextPage(nextPage + 1)
-  
+  const [nextPage, setNextPage] = useState(1);
+
+  const updatePage = () => setNextPage(nextPage + 1);
+
   useEffect(() => {
     dispatch(getUserListThunk({ perPage: 15, page: nextPage }));
   }, [nextPage]);
-  
+
   return (
-    <>
-    <InfiniteScroll
-      style={{height: "auto", overflow: "hidden"}}
-      dataLength={members.length}
-      next={updatePage}
-      loader={<h4>Get a real loader...</h4>}
-      hasMore={true}
+    <div>
+      <div>
+        <label htmlFor="">search</label>
+        <input type="text" />
+
+        <label htmlFor="">tipo</label>
+        <select>
+          <option value="name">nome</option>
+          <option value="tech">tech</option>
+        </select>
+      </div>
+      <InfiniteScroll
+        style={{ height: "auto", overflow: "hidden" }}
+        dataLength={members.length}
+        next={updatePage}
+        loader={<h4>Get a real loader...</h4>}
+        hasMore={true}
       >
         <MemberList>
-        {members.map(({ name, id, course_module,avatar_url, techs }, index) => (
-          <MemberCard
-            key={index}
-            name={name}
-            id={id}
-            course={course_module}
-            _MemberImg={avatar_url}
-            userTechs={techs}
-            avatar={avatar_url}
-          ></MemberCard>
-          ))}
-          </MemberList>
+          {members.map(
+            ({ name, id, course_module, avatar_url, techs }, index) => (
+              <MemberCard
+                key={index}
+                name={name}
+                id={id}
+                course={course_module}
+                _MemberImg={avatar_url}
+                userTechs={techs}
+                avatar={avatar_url}
+              ></MemberCard>
+            )
+          )}
+        </MemberList>
       </InfiniteScroll>
-    </>
+    </div>
   );
 };
 
