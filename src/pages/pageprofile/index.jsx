8bb466808f1801297f5b8c33a85da97851c26206
kevin wischneski <kevinwischneski@gmail.com>
2020-12-15 08:30:28 -0300

    Page now can change from works to techs and from techs to works

--- src/pages/pageprofile/index.jsx
+++ src/pages/pageprofile/index.jsx
@@ -9,9 +9,12 @@ import { getMyProfile } from "../../kenzieHub/user/myProfile";
 import TechForm from "../../components/forms/techForm";
 import WorkForm from "../../components/forms/workForm";
 
-const PageProfile = () => {
-  const [techs, settechs] = useState([]);
+import { FormControlLabel, Radio, RadioGroup } from "@material-ui/core";
 
+const PageProfile = () => {
+  const [techs, setTechs] = useState([]);
+  const [works, setWorks] = useState([]);
+  const [radioValue, setRadioValue] = useState("Tech");
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getMyProfile());
@@ -20,13 +23,13 @@ const PageProfile = () => {
   const { searchedMember } = useSelector((state) => state.members);
 
   useEffect(() => {
-    settechs(searchedMember.techs);
+    setTechs(searchedMember.techs);
+    setWorks(searchedMember.works);
   }, [searchedMember]);
 
   const sendDispatch = () => {
     dispatch(getMyProfile());
   };
-
   return (
     <>
       <Container>
@@ -35,19 +38,50 @@ const PageProfile = () => {
           bio={searchedMember.bio}
           name={searchedMember.name}
         ></CardProfile>
-        <TechForm sendDispatch={sendDispatch} />
-        <WorkForm sendDispatch={sendDispatch} />
-        {techs?.map(({ status, title, id }, index) => {
-          return (
-            <ContainerCard key={index}>
-              <Carteditprofile
-                status={status}
-                title={title}
-                id={id}
-              ></Carteditprofile>
-            </ContainerCard>
-          );
-        })}
+        <RadioGroup
+          aria-label="view"
+          name="view"
+          value={radioValue}
+          onChange={(evt) => setRadioValue(evt.target.value)}
+        >
+          <FormControlLabel
+            value="Tech"
+            control={<Radio />}
+            label="Tecnologias"
+          />
+          <FormControlLabel value="Work" control={<Radio />} label="Projetos" />
+        </RadioGroup>
+        {radioValue === "Tech" ? (
+          <>
+            <TechForm sendDispatch={sendDispatch} />
+            {techs?.map(({ status, title, id }, index) => {
+              return (
+                <ContainerCard key={index}>
+                  <Carteditprofile
+                    status={status}
+                    title={title}
+                    id={id}
+                  ></Carteditprofile>
+                </ContainerCard>
+              );
+            })}
+          </>
+        ) : (
+          <>
+            <WorkForm sendDispatch={sendDispatch} />
+            {works?.map(({ status, title, id }, index) => {
+              return (
+                <ContainerCard key={index}>
+                  <Carteditprofile
+                    status={status}
+                    title={title}
+                    id={id}
+                  ></Carteditprofile>
+                </ContainerCard>
+              );
+            })}
+          </>
+        )}
       </Container>
     </>
   );
